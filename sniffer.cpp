16c92ce9e78f96f01a885bd3a05b887bab680c83
Aditya Patil <tehblast007@gmail.com>
2026-05-05 15:15:35 +0000

    added IPv6 parser

--- sniffer.cpp
+++ sniffer.cpp
@@ -31,7 +31,18 @@ struct IPV4Header {
     uint32_t dest_ip;          // Dest IP (Big Endian)
 };
 
+struct IPV6Header {
+    uint32_t ver_tc_fl;       // Version, Traffic Class, Flow Label
+    uint16_t payload_len;     // Length of the data following this header
+    uint8_t  next_header;     // Next header type (TCP=6, UDP=17, ICMPv6=58)
+    uint8_t  hop_limit;       // Similar to TTL
+    std::array<uint8_t, 16> src_ip;
+    std::array<uint8_t, 16> dest_ip;
+};
+
 #pragma pack(pop)
+
+// --- Helper Functions ---
 
 int open_tap_device(const char* dev_name){
     struct ifreq ifr;
@@ -62,11 +73,20 @@ void print_mac(const std::array<uint8_t,6>& mac){
     }
 }
 
-void print_ip(uint32_t ip){
+void print_ipv4(uint32_t ip){
     uint8_t bytes[4];
     std::memcpy(bytes,&ip,4);
     std::cout<<std::dec<<(int)bytes[0]<<"."<<(int)bytes[1]<<"."<<(int)bytes[2]<<"."<<(int)bytes[3];
 }
+
+void print_ipv6(const std::array<uint8_t,16> &ip){
+    for(size_t i=0;i<16;i+=2){
+        std::cout<<std::hex<<std::setw(2)<<std::setfill('0')<<(int)ip[i]<<std::setw(2)<<std::setfill('0')<<(int)ip[i+1];
+        if(i<14)std::cout<<":";
+    }
+}
+
+// --- Main Logic ---
 
 int main(){
     const char* dev_name = "tap0";
@@ -99,18 +119,29 @@ int main(){
 
             // 2. Layer 3: IPv4
             // Identify the payload
-            if (eth.ether_type == 0x08000) {    //IPv4 Protocol
-                std::span<const uint8_t> ip_payload = packet.subspan(sizeof(EthernetHeader));
+            if (eth.ether_type == 0x0800) {    //IPv4 Protocol
+                std::span<const uint8_t> payload = packet.subspan(sizeof(EthernetHeader));
 
-                if(ip_payload.size() >= sizeof(IPV4Header)){
-                    IPV4Header ip;
-                    std::memcpy(&ip,ip_payload.data(),sizeof(IPV4Header));
+                if(payload.size() >= sizeof(IPV4Header)){
+                    IPV4Header ip4;
+                    std::memcpy(&ip4,payload.data(),sizeof(IPV4Header));
 
-                    std::cout<<" [IPv4] ";print_ip(ip.src_ip);std::cout<<" -> ";print_ip(ip.dest_ip);
-                    std::cout << " | proto: "<<std::dec<<(int)ip.protocol<<"\n";
+                    std::cout<<" [IPv4] ";print_ipv4(ip4.src_ip);std::cout<<" -> ";print_ipv4(ip4.dest_ip);
+                    std::cout << " | proto: "<<std::dec<<(int)ip4.protocol<<"\n";
                 }
             }
-            else if(eth.ether_type==0x0806){
+            else if(eth.ether_type == 0x86dd){  //IPv6 Protocol
+                std::span<const uint8_t> payload = packet.subspan(sizeof(EthernetHeader));
+
+                if(payload.size() >= sizeof(IPV6Header)){
+                    IPV6Header ip6;
+                    std::memcpy(&ip6,payload.data(),sizeof(IPV6Header));
+
+                    std::cout<<" [IPv6] ";print_ipv6(ip6.src_ip);std::cout<<" -> ";print_ipv6(ip6.dest_ip);
+                    std::cout<<" | Next Header: "<< std::dec << (int)ip6.next_header << "\n";
+                }
+            }
+            else if(eth.ether_type == 0x0806){
                 std::cout<<" [ARP] Request/Reply detection\n";
             }
             std::cout << "------------------------------------------\n\n";
